401f647e49bfa931eb3ec72716f24b2e21981c93
Oxana Grishchenko <grishchenko@buroburo.ru>
2020-01-07 18:02:24 +0200

    Updates

--- precache-manifest.f8c5f2509c9e59c307a82cbf9d094a87.js
+++ precache-manifest.f8c5f2509c9e59c307a82cbf9d094a87.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a6b3dc06cb8d8b09346f926312448ee3",
+    "revision": "30a9c88b2cae84af9d541451cc14a15c",
     "url": "/pwatest/index.html"
   },
   {
-    "revision": "be3dfa15a1462b18f5c4",
+    "revision": "783e75d01ca3838fe3c6",
     "url": "/pwatest/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "b0c6f7d738ff6315e3fc",
-    "url": "/pwatest/static/js/2.7cd09557.chunk.js"
+    "revision": "8e9c890a320df47cf566",
+    "url": "/pwatest/static/js/2.528a8102.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/pwatest/static/js/2.7cd09557.chunk.js.LICENSE"
+    "url": "/pwatest/static/js/2.528a8102.chunk.js.LICENSE"
   },
   {
-    "revision": "be3dfa15a1462b18f5c4",
-    "url": "/pwatest/static/js/main.99a6ba5b.chunk.js"
+    "revision": "783e75d01ca3838fe3c6",
+    "url": "/pwatest/static/js/main.81554e23.chunk.js"
   },
   {
     "revision": "0103ee05a0364e593ffc",
