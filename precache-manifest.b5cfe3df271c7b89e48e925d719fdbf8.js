e339b3f4aebe9e226f66b0eedf48e30b9f752d7e
Oxana Grishchenko <grishchenko@buroburo.ru>
2020-01-07 15:42:57 +0200

    Updates

---
+++ precache-manifest.b5cfe3df271c7b89e48e925d719fdbf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c6991407068cd4aa9a8fdb30d612ba",
+    "url": "/pwatest/index.html"
+  },
+  {
+    "revision": "5d7e63319a73796e4052",
+    "url": "/pwatest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b0c6f7d738ff6315e3fc",
+    "url": "/pwatest/static/js/2.7cd09557.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pwatest/static/js/2.7cd09557.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d7e63319a73796e4052",
+    "url": "/pwatest/static/js/main.f9d122a9.chunk.js"
+  },
+  {
+    "revision": "0103ee05a0364e593ffc",
+    "url": "/pwatest/static/js/runtime-main.dc4b96ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwatest/static/media/logo.5d5d9eef.svg"
+  }
+]);
